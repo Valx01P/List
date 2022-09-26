d95ba2ab7fa05007aacc623ecf2d2254995a0a9e
Pablo Valdes <pablovaldes0925@gmail.com>
2022-09-26 04:39:54 -0400

    made list dynamic

--- app.js
+++ app.js
@@ -4,46 +4,36 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+//item prototype
+var items = ["Buy Food", "Cook Food", "Eat Food"];
 //ejs is a template for express so it must go below it
 app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/", function(req, res) {
 //there is actually a function that tells us the day of the week sun-sat (0-6)
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
-//this is better than using and if else statment, switch statement for more
-  switch (currentDay) {
-    case 0:
-      day = "Sunday"
-      break;
-    case 1:
-      day = "Monday"
-      break;
-    case 2:
-      day = "Tuesday"
-      break;
-    case 3:
-      day = "Wednesday"
-      break;
-    case 4:
-      day = "Thursday"
-      break;
-    case 5:
-      day = "Friday"
-      break;
-    case 6:
-      day = "Saturday"
-      break;
-      //default statement should typically not occur
-    default:
-    console.log("Error: current day is equal to: " + currentDay);
-  }
-//without ejs we would not be able to do this
-  res.render("list", {
-    kindOfDay: day
-  });
 
+var options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long"
+};
+
+var day = today.toLocaleDateString("en-US", options);
+
+//without ejs we would not be able to do this
+  res.render("list", {kindOfDay: day, newListItems: items});
+
+});
+
+app.post("/", function(req, res){
+  var item = req.body.newItem;
+
+  items.push(item);
+
+  res.redirect("/");
 });
 
 app.listen(3000, function() {
